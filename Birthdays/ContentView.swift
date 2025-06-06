96d7b9a0dc03fd0eb14aed3c9d66d279fcbdb8db
Annie <annenark@gmail.com>
2025-06-06 10:36:21 -0500

    Added a birthday function which allows you to add birthdays to a list

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    @State private var friends: [Friend] = [
+        Friend(name: "Estelle Palleta", birthday: Date(timeIntervalSince1970: 1243382400.0)),
+        Friend(name: "Hugh Mann", birthday: Date(timeIntervalSince1970: 1000000000))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+                
+            }
+            .navigationTitle(Text("Birthdays"))
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                    .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        
+                        if newName != "" {
+                            friends.append(newFriend)
+                            newName = ""
+                            newBirthday = .now
+                            }
+                            }
+                            .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
+            
         }
-        .padding()
     }
 }
 
